005f3bfa7aa51a87b4f4237113e56e7f415ef8d9
JohnAndrewPurio <lyerdestroyer@gmail.com>
2022-06-09 21:06:46 +0800

    Added watch flag

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   apps : [{
     name   : "rest-node-firmware",
-    script : "npm run build && npm run start:pi"
+    script : "npm run build && npm run start:pi",
+    watch: true,
+    ignore_watch: [
+        "node_modules",
+        "dist",
+        "static"
+    ]
   }]
 }
