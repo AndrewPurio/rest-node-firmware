31f85d4b8529b32daa20251b2d8a43b6b199b6ee
John <john@existtribe.com>
2022-07-20 12:32:37 +0800

    Added debounce reset button

--- src/routes/system/reset/index.ts
+++ src/routes/system/reset/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { resetHotspotConfig } from "../../../utils/network/access_point";
 import { resetDevice } from "../../../utils/system";
 
 const router = Router()
 
 router.get("/", async (request, response) => {
     try {
+        await resetHotspotConfig()
         await resetDevice()
 
         response.json({
