d6a3e5c70b1768ded44f38b378c9c5a8388178cf
John <john@existtribe.com>
2022-07-27 12:33:20 +0800

    Updated url for audio effects

--- src/utils/sound/constants.ts
+++ src/utils/sound/constants.ts
@@ -1,4 +1,4 @@
 export enum SoundEffect {
-    ON_START = "../../static/.sound_effects/bell.wav",
-    ON_RESET = "../../static/.sound_effects/Exist Tribe Chime.mp4"
+    ON_START = "http://localhost/sound/static/.sound_effects/bell.wav",
+    ON_RESET = "http://localhost/sound/static/.sound_effects/Exist Tribe Chime.mp4"
 }
