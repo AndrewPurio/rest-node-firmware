66a7bb2452d21d5997f4d60fb825184f7b2d5d15
John <john@existtribe.com>
2022-07-27 12:09:55 +0800

    Updated try clause

--- src/utils/sound/index.ts
+++ src/utils/sound/index.ts
@@ -5,33 +5,28 @@ import { playMedia } from "../vlc"
 import { SoundEffect } from "./constants"
 
 export const playSoundEffect = async (sound: SoundEffect) => {
-    const { stdout: currentVolume } = await playerctl(MediaPlayerEvent.volume)
+    let currentVolume = 80
+
+    try {
+        const { stdout } = await playerctl(MediaPlayerEvent.volume)
+
+        currentVolume = stdout
+    } finally { }
+
     const spawn = playMedia(sound, {
         playAndExit: true,
         noVideo: true
     })
 
-    await delay(100)
-
-    console.log("Media Spawn:", spawn)
-
-    try {
-        playerctl(MediaPlayerEvent.volume, {
-            value: 80
-        })
-    } catch (error) {
-        console.log(error)
-    }
+    playerctl(MediaPlayerEvent.volume, {
+        value: 80
+    })
 
     spawn.addListener("close", () => {
         console.log("Sound effect playing has stopped")
 
-        try {
-            playerctl(MediaPlayerEvent.volume, {
-                value: currentVolume
-            })
-        } catch (error) {
-            console.log(error)
-        }
+        playerctl(MediaPlayerEvent.volume, {
+            value: currentVolume
+        })
     })
 }
